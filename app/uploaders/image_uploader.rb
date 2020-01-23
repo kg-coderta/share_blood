37b74e578ab3a33d910c85dbea07fb3d4b8dc1f7
kg <ke123iji@gmail.com>
2020-01-24 07:33:41 +0900

    add default article image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,12 +17,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
+    "default.jpg"
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -45,6 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "something.jpg" if original_filename
+    original_filename if original_filename
   end
 end
