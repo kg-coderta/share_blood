6cf5627082f6d067568aa00e44ba5c118fc74647
kg <ke123iji@gmail.com>
2020-01-24 07:34:27 +0900

    validate  and make association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+  
+  belongs_to :user
+  has_many :comments
+
+  mount_uploader :image, ImageUploader
+
+  validates :user_id, presence: true
+  validates :text, presence: true
 end
