615bc92ae6cdd1c9ec18c85414aa4df1250e2b90
kg <ke123iji@gmail.com>
2020-03-23 07:53:07 +0900

    modify validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+
+  validates :nickname, presence: true, length: { maximum: 15 }     
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i        
+  validates :email, presence: true, uniqueness: true, format:{ with: VALID_EMAIL } 
+  validates :password, presence: true, length: { in: 6..128 }
+
   
   has_many :articles
   has_many :comments
